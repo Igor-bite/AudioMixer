d5ca70076c204962d72c265373fe4a7b2e062242
Igor Klyuzhev <klyuzhevigor@gmail.com>
2023-12-02 11:08:51 +0600

    chore: Added shape assets

--- AudioMixer/Resources/Generated/Assets.swift
+++ AudioMixer/Resources/Generated/Assets.swift
@@ -22,8 +22,27 @@ internal typealias AssetImageTypeAlias = ImageAsset.Image
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 internal enum Asset {
+  internal enum Shapes {
+    internal static let circleFilled = ImageAsset(name: "Shapes/circle-filled")
+    internal static let circleOutline = ImageAsset(name: "Shapes/circle-outline")
+    internal static let curl1 = ImageAsset(name: "Shapes/curl-1")
+    internal static let curl2 = ImageAsset(name: "Shapes/curl-2")
+    internal static let curl3 = ImageAsset(name: "Shapes/curl-3")
+    internal static let dots = ImageAsset(name: "Shapes/dots")
+    internal static let linesDiagonal = ImageAsset(name: "Shapes/lines-diagonal")
+    internal static let linesHorizontal = ImageAsset(name: "Shapes/lines-horizontal")
+    internal static let linesStrange = ImageAsset(name: "Shapes/lines-strange")
+    internal static let sunFilled = ImageAsset(name: "Shapes/sun-filled")
+    internal static let sunOutline = ImageAsset(name: "Shapes/sun-outline")
+    internal static let sun = ImageAsset(name: "Shapes/sun")
+    internal static let tetraedrFilled = ImageAsset(name: "Shapes/tetraedr-filled")
+    internal static let tetraedrGreen = ImageAsset(name: "Shapes/tetraedr-green")
+    internal static let tetraedrWhite = ImageAsset(name: "Shapes/tetraedr-white")
+  }
+
   internal static let chevronUp = ImageAsset(name: "chevron_up")
   internal static let cross = ImageAsset(name: "cross")
+  internal static let downloadArrow = ImageAsset(name: "download_arrow")
   internal static let drums = ImageAsset(name: "drums")
   internal static let guitar = ImageAsset(name: "guitar")
   internal static let launchScreenImage = ImageAsset(name: "launch_screen_image")
